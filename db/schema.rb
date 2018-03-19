812ecd8a4f591ae581232e26d0879e9b26808dc2
Le Hoang Tam <kokoro.lehoang@gmail.com>
2018-03-19 11:44:12 +0700

    add table category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319023450) do
+ActiveRecord::Schema.define(version: 20180319044342) do
 
   create_table "domains", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
